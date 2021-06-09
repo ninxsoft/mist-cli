4673793db02de149e50f08e9b59656aa3c6140b6
Nindi Gill <nindi@ninxsoft.com>
2021-06-09 20:59:11 +1000

    Removed verbosity of messaging

--- Mist/Helpers/PrettyPrint.swift
+++ Mist/Helpers/PrettyPrint.swift
@@ -10,34 +10,19 @@ import Foundation
 struct PrettyPrint {
 
     enum PrintType: String {
-        case info = "INFO"
-        case warning = "WARN"
-        case error = "ERROR"
+        case info = "✅"
+        case warning = "⚠️"
+        case error = "⛔️"
 
         var identifier: String {
             rawValue
-        }
-
-        var color: String.Color {
-            switch self {
-            case .info:
-                return .green
-            case .warning:
-                return .yellow
-            case.error:
-                return .red
-            }
-        }
-
-        var descriptionWithColor: String {
-            identifier.color(self.color)
         }
     }
 
     static func print(_ type: PrintType, prefix: Bool = true, string: String, carriageReturn: Bool = false, newLine: Bool = true) {
         let carriageReturn: String = carriageReturn ? "\r" : ""
         let terminator: String = newLine ? "\n" : ""
-        let string: String = "\(prefix ? "[\(type.descriptionWithColor)] \(Date()) - \(string)" : string)\(carriageReturn)"
+        let string: String = "\(prefix ? "\(type.identifier) \(string)" : string)\(carriageReturn)"
         Swift.print(string, terminator: terminator)
     }
 }
