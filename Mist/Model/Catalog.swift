ead550817c3af3c763e4b274b688d58d651cd110
Nindi Gill <nindi@ninxsoft.com>
2021-06-09 23:07:08 +1000

    Added macOS Monterey SUCatalog

--- Mist/Model/Catalog.swift
+++ Mist/Model/Catalog.swift
@@ -31,12 +31,16 @@ enum Catalog: String, ExpressibleByArgument {
     var urls: [String] {
         let prefix: String = "https://swscan.apple.com/content/catalogs/others/index-"
         let slugs: [String] = [
+            "12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog",
             "10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog",
             "10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog",
             "10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog",
             "10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog"
         ]
 
-        return slugs.map { "\(prefix)\(self.description.isEmpty ? "" : $0.prefix(5))\(self.description)\($0)" }
+        return slugs.map {
+            let string: String = self == .standard ? "" : "\($0.prefix($0.firstIndex(of: "-")?.utf16Offset(in: $0) ?? 5))"
+            return "\(prefix)\(string)\(self.description)\($0)"
+        }
     }
 }
