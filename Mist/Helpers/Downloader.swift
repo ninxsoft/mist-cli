38e25127c811c658a1a74c9d44fa1af4e27265ee
Nindi Gill <nindi@ninxsoft.com>
2021-06-09 20:59:49 +1000

    Removed download progress output

--- Mist/Helpers/Downloader.swift
+++ Mist/Helpers/Downloader.swift
@@ -7,116 +7,61 @@
 
 import Foundation
 
-class Downloader: NSObject {
+struct Downloader {
 
     private let semaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
-    private var temporaryURL: URL = URL(fileURLWithPath: "")
-    private var source: URL = URL(fileURLWithPath: "")
-    private var prefix: String = ""
-    private var totalBytesWritten: Int64 = 0
-    private var totalBytesExpectedToWrite: Int64 = 0
-    private var formattedTotalBytesWritten: String {
-        totalBytesWritten.formattedCapacity()
-    }
-    private var formattedTotalBytesExpectedToWrite: String {
-        let value: Int64 = totalBytesExpectedToWrite > 0 ? totalBytesExpectedToWrite : totalBytesWritten
-        return value.formattedCapacity()
-    }
-    private var percent: String {
-
-        let blank: String = "      "
-
-        guard totalBytesExpectedToWrite > 0 else {
-            return blank
-        }
-
-        let value: Double = Double(totalBytesWritten) / Double(totalBytesExpectedToWrite) * 100
-
-        guard value < 100 else {
-            return "100.0%"
-        }
-
-        let numberFormatter: NumberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.minimumIntegerDigits = 2
-        numberFormatter.maximumIntegerDigits = 2
-        numberFormatter.minimumFractionDigits = 2
-        numberFormatter.maximumFractionDigits = 2
-        let number: NSNumber = NSNumber(value: value)
-
-        guard let string: String = numberFormatter.string(from: number) else {
-            return blank
-        }
-
-        return "\(string)%"
-    }
 
     func download(_ product: Product) throws {
 
-        let session: URLSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
-
-        temporaryURL = URL(fileURLWithPath: "\(String.baseTemporaryDirectory)/\(product.identifier)")
+        let temporaryURL: URL = URL(fileURLWithPath: "\(String.baseTemporaryDirectory)/\(product.identifier)")
         let urls: [String] = [product.distribution] + product.packages.map { $0.url }.sorted { $0 < $1 }
 
         try FileManager.default.remove(temporaryURL, description: "old temporary directory")
         try FileManager.default.create(temporaryURL, description: "new temporary directory")
 
         for (index, url) in urls.enumerated() {
-            guard let sourceURL: URL = URL(string: url) else {
+            guard let source: URL = URL(string: url) else {
                 throw MistError.invalidURL(url: url)
             }
 
-            source = sourceURL
+            let string: String = "[\(index + 1) / \(product.totalFiles)] Downloading \(source.lastPathComponent)..."
+            PrettyPrint.print(.info, string: string)
 
-            let type: String = PrettyPrint.PrintType.info.descriptionWithColor
-            let date: Date = Date()
-            let dateFormatter: DateFormatter = DateFormatter()
-            dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
-            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
-            prefix = String(format: "[%@] %@ - [%02d / %02d] Downloading %@...", type, dateFormatter.string(from: date), index + 1, product.totalFiles, source.lastPathComponent)
-            PrettyPrint.print(.info, prefix: false, string: prefix, carriageReturn: true, newLine: false)
+            let task: URLSessionDownloadTask = URLSession.shared.downloadTask(with: source) { url, response, error in
 
-            let task: URLSessionDownloadTask = session.downloadTask(with: source)
-            totalBytesWritten = 0
-            totalBytesExpectedToWrite = 0
+                if let error: Error = error {
+                    PrettyPrint.print(.error, string: error.localizedDescription)
+                    exit(1)
+                }
+
+                guard let response: HTTPURLResponse = response as? HTTPURLResponse else {
+                    PrettyPrint.print(.error, string: "There was an error retrieving \(source.lastPathComponent)")
+                    exit(1)
+                }
+
+                guard response.statusCode == 200 else {
+                    PrettyPrint.print(.error, string: "Invalid HTTP status code: \(response.statusCode)")
+                    exit(1)
+                }
+
+                guard let location: URL = url else {
+                    PrettyPrint.print(.error, string: "Invalid temporary URL")
+                    exit(1)
+                }
+
+                let destination: URL = temporaryURL.appendingPathComponent(source.lastPathComponent)
+
+                do {
+                    try FileManager.default.move(location, to: destination)
+                    semaphore.signal()
+                } catch {
+                    PrettyPrint.print(.error, string: error.localizedDescription)
+                    exit(1)
+                }
+            }
+
             task.resume()
             semaphore.wait()
-        }
-    }
-}
-
-extension Downloader: URLSessionDownloadDelegate {
-
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-
-        if let error: Error = error {
-            PrettyPrint.print(.error, string: error.localizedDescription)
-            exit(1)
-        }
-
-        let suffix: String = "[\(formattedTotalBytesWritten) of \(formattedTotalBytesExpectedToWrite) - 100.0%]".color(.green)
-        let string: String = prefix + suffix
-        PrettyPrint.print(.info, prefix: false, string: string)
-        semaphore.signal()
-    }
-
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        self.totalBytesWritten = totalBytesWritten
-        self.totalBytesExpectedToWrite = totalBytesExpectedToWrite
-        let suffix: String = "[\(formattedTotalBytesWritten) of \(formattedTotalBytesExpectedToWrite) - \(percent)]".color(.blue)
-        let string: String = prefix + suffix
-        PrettyPrint.print(.info, prefix: false, string: string, carriageReturn: true, newLine: false)
-    }
-
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-
-        let destination: URL = temporaryURL.appendingPathComponent(source.lastPathComponent)
-
-        do {
-            try FileManager.default.move(location, to: destination)
-        } catch {
-            PrettyPrint.print(.error, string: error.localizedDescription)
-            exit(1)
         }
     }
 }
