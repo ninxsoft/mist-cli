7a76a6c2085480bd3bf2e5cfb568026664952862
Nindi Gill <nindi@ninxsoft.com>
2022-07-09 13:06:42 +1000

    Replace version subcommand with built-in --version

--- Mist/Commands/Mist.swift
+++ Mist/Commands/Mist.swift
@@ -9,9 +9,45 @@ import ArgumentParser
 import Foundation
 
 struct Mist: ParsableCommand {
-    static let configuration: CommandConfiguration = CommandConfiguration(abstract: .abstract, discussion: .discussion, subcommands: [ListCommand.self, DownloadCommand.self, VersionCommand.self])
+    static let configuration: CommandConfiguration = CommandConfiguration(abstract: .abstract, discussion: .discussion, version: version(), subcommands: [ListCommand.self, DownloadCommand.self])
+
+    /// Current version.
+    private static let currentVersion: String = "1.8"
+    /// Current version with error message when unable to lookup latest version.
+    private static var versionWithErrorMessage: String {
+        "\(currentVersion) (Unable to check for latest version)"
+    }
 
     static func noop() { }
+
+    static func version() -> String {
+
+        guard let url: URL = URL(string: .latestReleaseURL) else {
+            return versionWithErrorMessage
+        }
+
+        do {
+            let string: String = try String(contentsOf: url, encoding: .utf8)
+
+            guard let data: Data = string.data(using: .utf8),
+                let dictionary: [String: Any] = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
+                let tag: String = dictionary["tag_name"] as? String else {
+                return versionWithErrorMessage
+            }
+
+            let latestVersion: String = tag.replacingOccurrences(of: "v", with: "")
+            var versionString: String = "\(currentVersion) (Latest: \(latestVersion))"
+
+            guard currentVersion.compare(latestVersion, options: .numeric) == .orderedAscending else {
+                return versionString
+            }
+
+            versionString += "\nVisit \(String.repositoryURL) to grab the latest release of \(String.appName)"
+            return versionString
+        } catch {
+            return versionWithErrorMessage
+        }
+    }
 
     mutating func run() throws {
         print(Mist.helpMessage())
