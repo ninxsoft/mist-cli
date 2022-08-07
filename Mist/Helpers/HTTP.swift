6bd6753e0f1bbdf93591af0d1b0ac097dcb954ab
Nindi Gill <nindi@ninxsoft.com>
2022-08-07 14:40:57 +1000

    Fix version sorting (by date)

--- Mist/Helpers/HTTP.swift
+++ Mist/Helpers/HTTP.swift
@@ -75,7 +75,7 @@ struct HTTP {
             firmwares = firmwares.filter { $0.compatible }
         }
 
-        firmwares.sort { $0.version == $1.version ? ($0.build.count == $1.build.count ? $0.build > $1.build : $0.build.count > $1.build.count) : $0.version > $1.version }
+        firmwares.sort { $0.version == $1.version ? $0.date > $1.date : $0.version > $1.version }
         return firmwares
     }
 
@@ -158,7 +158,7 @@ struct HTTP {
             products = products.filter { $0.compatible }
         }
 
-        products.sort { $0.version == $1.version ? ($0.build.count == $1.build.count ? $0.build > $1.build : $0.build.count > $1.build.count) : $0.version > $1.version }
+        products.sort { $0.version == $1.version ? $0.date > $1.date : $0.version > $1.version }
         return products
     }
 
