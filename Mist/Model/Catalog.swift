ea3bd742785a56eb55d25c03655fcdf6ed2a7d7b
Nindi Gill <nindi@ninxsoft.com>
2023-06-06 21:31:58 +1000

    Add support for macOS 14 Sonoma

--- Mist/Model/Catalog.swift
+++ Mist/Model/Catalog.swift
@@ -18,14 +18,13 @@ enum Catalog: String, CaseIterable {
     var url: String {
         switch self {
         case .standard:
-            return "https://swscan.apple.com/content/catalogs/others/index-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
-        case .customer:
-            return
-                "https://swscan.apple.com/content/catalogs/others/index-13customerseed-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
+            return "https://swscan.apple.com/content/catalogs/others/index-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
+        case .customer: // swiftlint:disable:next line_length
+            return "https://swscan.apple.com/content/catalogs/others/index-14customerseed-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
         case .developer:
-            return "https://swscan.apple.com/content/catalogs/others/index-13seed-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
+            return "https://swscan.apple.com/content/catalogs/others/index-14seed-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
         case .`public`:
-            return "https://swscan.apple.com/content/catalogs/others/index-13beta-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
+            return "https://swscan.apple.com/content/catalogs/others/index-14beta-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
         }
     }
 }
--- Mist/Model/Firmware.swift
+++ Mist/Model/Firmware.swift
@@ -30,7 +30,9 @@ struct Firmware: Decodable {
 
         var name: String = ""
 
-        if version.range(of: "^13", options: .regularExpression) != nil {
+        if version.range(of: "^14", options: .regularExpression) != nil {
+            name = "macOS Sonoma"
+        } else if version.range(of: "^13", options: .regularExpression) != nil {
             name = "macOS Ventura"
         } else if version.range(of: "^12", options: .regularExpression) != nil {
             name = "macOS Monterey"
