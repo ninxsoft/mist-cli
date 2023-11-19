c090fed6a661fd69640a305466a71c28a7bcaafc
Nindi Gill <nindi@ninxsoft.com>
2023-11-19 18:46:09 +1100

    SwiftFormat redundantBackticks

--- Mist/Model/Catalog.swift
+++ Mist/Model/Catalog.swift
@@ -23,7 +23,7 @@ enum Catalog: String, CaseIterable {
             "https://swscan.apple.com/content/catalogs/others/index-14customerseed-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
         case .developer:
             "https://swscan.apple.com/content/catalogs/others/index-14seed-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
-        case .`public`:
+        case .public:
             "https://swscan.apple.com/content/catalogs/others/index-14beta-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
         }
     }
