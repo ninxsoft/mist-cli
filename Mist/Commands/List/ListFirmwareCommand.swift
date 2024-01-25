ef064a59eb1e6ab2e3931226fc1129c1ad088516
Nindi Gill <nindi@ninxsoft.com>
2024-01-25 15:23:08 +1100

    Export URLs correctly (#158)
    * Export the correct dictionary
    
    * Tidy up CSV exports

--- Mist/Commands/List/ListFirmwareCommand.swift
+++ Mist/Commands/List/ListFirmwareCommand.swift
@@ -48,7 +48,7 @@ struct ListFirmwareCommand: ParsableCommand {
             }
         }
 
-        try export(firmwares.map(\.dictionary), options: options)
+        try export(firmwares.map(\.exportDictionary), options: options)
         !options.quiet ? PrettyPrint.print("Found \(firmwares.count) macOS Firmware(s) available for download\n", noAnsi: options.noAnsi, prefix: .ending) : Mist.noop()
 
         if !firmwares.isEmpty {
