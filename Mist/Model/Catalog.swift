c5f5daab670a4b0b425cd735eaff8a09003c6363
Nindi Gill <nindi@ninxsoft.com>
2026-06-10 09:13:22 +1000

    Update Catalog.swift

--- Mist/Model/Catalog.swift
+++ Mist/Model/Catalog.swift
@@ -31,7 +31,7 @@ enum Catalog: String, CaseIterable {
             "https://swscan.apple.com/content/catalogs/others/index-26beta-26-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
         }
     }
-    
+
     private var goldenGateURL: String {
         switch self {
         case .standard:
